721bafaccfc5c9ceff784122ef7efcc77b3b14a3
Juliano Martinez <juliano@martinez.io>
2013-08-29 17:41:57 -0300

    spliting and cleaning code

--- hpr.go
+++ hpr.go
@@ -13,8 +13,6 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 
-      Based on http://github.com/nf/webfront
-
    @author: Juliano Martinez
 */
 
@@ -23,6 +21,7 @@ package main
 import (
 	"crypto/tls"
 	"github.com/fiorix/go-redis/redis"
+	hpr_http_server "github.com/ncode/hot-potato-router/http_server"
 	hpr_utils "github.com/ncode/hot-potato-router/utils"
 	"log"
 	"net"
@@ -35,20 +34,8 @@ import (
 	"time"
 )
 
-type Server struct {
-	mu    sync.RWMutex
-	last  time.Time
-	proxy map[string]http.Handler
-}
-
-type Proxy struct {
-	Backend string
-	handler http.Handler
-}
-
 var (
 	cfg = hpr_utils.NewConfig()
-	rc  = redis.New(cfg.Options["redis"]["server_list"])
 )
 
 func main() {
@@ -56,7 +43,7 @@ func main() {
 	if probe_interval == 0 {
 		probe_interval = 10
 	}
-	s, err := NewServer(time.Duration(probe_interval) * time.Second)
+	s, err := hpr_http_server.NewServer(time.Duration(probe_interval) * time.Second)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -68,75 +55,10 @@ func main() {
 			log.Fatal(err)
 		}
 		c := &tls.Config{Certificates: []tls.Certificate{cert}}
-		l := tls.NewListener(listen(https_fd, cfg.Options["hpr"]["https_addr"]), c)
+		l := tls.NewListener(hpr_http_server.Listen(https_fd, cfg.Options["hpr"]["https_addr"]), c)
 		go func() {
 			log.Fatal(http.Serve(l, s))
 		}()
 	}
-	log.Fatal(http.Serve(listen(http_fd, cfg.Options["hpr"]["http_addr"]), s))
-}
-
-func listen(fd int, addr string) net.Listener {
-	var l net.Listener
-	var err error
-	if fd >= 3 {
-		l, err = net.FileListener(os.NewFile(uintptr(fd), "http"))
-	} else {
-		l, err = net.Listen("tcp", addr)
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
-	return l
-}
-
-func NewServer(probe time.Duration) (*Server, error) {
-	s := new(Server)
-	s.proxy = make(map[string]http.Handler)
-	go s.probe_backends(probe)
-	return s, nil
-}
-
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if h := s.handler(r); h != nil {
-		h.ServeHTTP(w, r)
-		return
-	}
-	http.Error(w, "Not found.", http.StatusNotFound)
-}
-
-func (s *Server) handler(req *http.Request) http.Handler {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	h := req.Host
-	if i := strings.Index(h, ":"); i >= 0 {
-		h = h[:i]
-	}
-
-	_, ok := s.proxy[h]
-	if !ok {
-		v, _ := rc.Get(h)
-		s.proxy[h] = makeHandler(v)
-	}
-	return s.proxy[h]
-}
-
-func (s *Server) probe_backends(probe time.Duration) {
-	for {
-		s.mu.Lock()
-		s.mu.Unlock()
-		time.Sleep(probe)
-	}
-}
-
-func makeHandler(f string) http.Handler {
-	if f != "" {
-		return &httputil.ReverseProxy{
-			Director: func(req *http.Request) {
-				req.URL.Scheme = "http"
-				req.URL.Host = f
-			},
-		}
-	}
-	return nil
+	log.Fatal(http.Serve(hpr_http_server.Listen(http_fd, cfg.Options["hpr"]["http_addr"]), s))
 }
