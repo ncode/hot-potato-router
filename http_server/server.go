24832691d5377b44cda13eedee0dbed5bd4c3dae
Juliano Martinez <juliano@martinez.io>
2013-09-22 13:31:39 -0300

    debug de bahia no

--- http_server/server.go
+++ http_server/server.go
@@ -144,6 +144,7 @@ func (s *Server) populate_proxies(vhost string) (err error) {
 			s.vcount[vhost][backend]++
 			utils.Log(fmt.Sprintf("Backend %s with %d handlers for %s", backend, s.vcount[vhost][backend], vhost))
 		}
+		fmt.Println(s.vcount[vhost][backend])
 	}
 	return
 }
